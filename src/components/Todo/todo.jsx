70e784d1eac576f21e9271425bde6288d99f1fe0
Thyago Fontenele <thyagofontenele@id.uff.br>
2021-11-24 17:31:10 -0300

    checkbox personalizado completo

--- src/components/Todo/todo.jsx
+++ src/components/Todo/todo.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import classes from './todo.css'
-import Lixeira from '../../assets/icons/trash/trash.png'
-import Lixeira2x from '../../assets/icons/trash/trash@2x.png'
-export default function todo({title}){
+import './todo.css'
 
+import Lixeira from '../../assets/icons/trash/trash.png'
+export default function todo({title}){
+    
     return(
         <>
-            <li className={classes.container}>
-                <input type="checkbox" className={classes.check} />
-                <label>{title}</label>
-                <img src={Lixeira} alt="ExcluirTask" />
+            <li class='container'>
+
+                <label class='switch'>
+                    <input type="checkbox" name="checkbox" class='checkbox' content="d"/>
+                    <span class='text'>{title}</span>
+                </label>
+                <img src={Lixeira} alt="ExcluirTask" class='trash'/>
+                
+
             </li>
         </>
     )
