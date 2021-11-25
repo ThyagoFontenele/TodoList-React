3eabcde015106ed3a3a3a399ec44230f6b44b326
Thyago Fontenele <thyagofontenele@id.uff.br>
2021-11-25 20:16:41 -0300

    problema vercel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Header from '../components/Header/Header'
 import classes from './HomePage.module.css'
-import Todo from '../components/Todo/Todo'
+import Todo from '../components/Todo/Todo.jsx'
 
 
 export default function HomePage() {
