6fd287cd77b56cbd30dcfa258bb3d39e2e86d2f3
Thyago Fontenele <thyagofontenele@id.uff.br>
2021-11-25 04:02:32 -0300

    problema de checked resolvido

--- src/components/Todo/todo.jsx
+++ src/components/Todo/todo.jsx
@@ -7,25 +7,26 @@ export default function Todo({title, array, removerTarefa}){
 
     const [slowStyle, setSlowStyle] = useState();
     const estilo = {
-        transition: 'all 700ms',
+        transition: 'all 600ms',
         opacity: 0
     }
-
     return(
         <>
             <li class='container' style={slowStyle}>
 
                 <label class='switch'>
-                    <input type="checkbox" name="checkbox" class='checkbox' content="d"/>
+                    <input type="checkbox" name="checkbox" class='checkbox'/>
                     <span class='text'>{title}</span>
                 </label>
                 <img src={Lixeira} alt="ExcluirTask" class='trash' onClick={ () => { 
                     let index = array.indexOf(title);
                     setSlowStyle(estilo);
+                    let test = document.querySelectorAll('.checkbox');
+                    test[index].checked = false;
                     setTimeout(function() {
                         removerTarefa(index ,array)
                         setSlowStyle();
-                    }, 700);
+                    }, 500);
                 }}/>
 
             </li>
